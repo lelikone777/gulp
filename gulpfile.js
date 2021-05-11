dc3ead3c52684e24e854d9864519ff1d045a59ab
KALININ ALEXEY <2257855@gmail.com>
2021-05-11 12:26:02 +0300

    11/05 before mac

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,9 @@ let { src, dest } = require("gulp"),
   webp = require("gulp-webp"),
   webphtml = require("gulp-webp-html"),
   webpcss = require("gulp-webpcss"),
-  svgSprite = require("gulp-svg-sprite");
+  svgSprite = require("gulp-svg-sprite"),
+  ttf2woff = require("gulp-ttf2woff"),
+  ttf2woff2 = require("gulp-ttf2woff2");
 
 function browserSync() {
   browsersync.init({
@@ -123,6 +125,11 @@ function images() {
     .pipe(browsersync.stream());
 }
 
+function fonts() {
+  src(path.src.fonts).pipe(ttf2woff()).pipe(dest(path.build.fonts));
+  return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
+};
+
 gulp.task("svgSprite", function () {
   return gulp
     .src([source_folder + "/iconsprite/*.svg"])
@@ -131,7 +138,7 @@ gulp.task("svgSprite", function () {
         mode: {
           stack: {
             sprite: "../icons/icons.svg", // sprite file name
-            example: true
+            example: true,
           },
         },
       })
@@ -150,9 +157,10 @@ function clean() {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
